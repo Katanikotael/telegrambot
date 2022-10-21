f4268179a3a25a5b3eddf8507fd50ddd1f5f17fd
Patrick Bateman <jagg@ya.ru>
2022-10-21 20:04:03 +0300

    JRTB-2: added stub telegram bot to the project

--- src/main/java/com/github/katanikotael/jrtb/TelegramBotApplication.java
+++ src/main/java/com/github/katanikotael/jrtb/TelegramBotApplication.java
@@ -2,12 +2,17 @@ package com.github.katanikotael.jrtb;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.stereotype.Component;
 
 @SpringBootApplication
 public class TelegramBotApplication {
 
 	public static void main(String[] args) {
+
+//		System.getProperties().put("proxySet", "true");
+//		System.getProperties().put("socksProxyHost", "127.0.0.1");
+//		System.getProperties().put("socksProxyPort", "9150");
+
 		SpringApplication.run(TelegramBotApplication.class, args);
 	}
-
 }
---
+++ src/main/java/com/github/katanikotael/jrtb/bot/SapTelegramBot.java
@@ -0,0 +1,58 @@
+package com.github.katanikotael.jrtb.bot;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
+import org.telegram.telegrambots.bots.TelegramLongPollingBot;
+import org.telegram.telegrambots.meta.api.methods.send.SendMessage;
+import org.telegram.telegrambots.meta.api.objects.Update;
+import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
+
+/**
+ * My first telegram bot
+  */
+
+@Component
+public class SapTelegramBot extends TelegramLongPollingBot {
+
+    @Value("${bot.username}")
+    private String username;
+
+    @Value("${bot.token}")
+    private String token;
+
+    @Override
+    public String getBotUsername() {
+        return username;
+    }
+
+    @Override
+    public String getBotToken() {
+        return token;
+    }
+
+    @Override
+    public void onUpdateReceived(Update update) {
+        if (update.hasMessage() && update.getMessage().hasText()) {
+            String message = update.getMessage().getText().trim();
+            String chatId = update.getMessage().getChatId().toString();
+
+            System.out.println(update.getMessage().getFrom().getUserName() + ": " + message);
+
+            SendMessage sm = new SendMessage();
+            sm.setChatId(chatId);
+            if (message.toLowerCase().contains("привет")) {
+                sm.setText("И тебе привет!");
+            } else {
+                sm.setText(message);
+            }
+
+            try {
+                execute(sm);
+                System.out.println("bot: " + sm.getText());
+            } catch (TelegramApiException e) {
+                e.printStackTrace();
+            }
+        }
+
+    }
+}
